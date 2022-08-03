da999bc72d552eacd574f41049cdd3b398dafdbe
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-08-03 16:20:55 +0800

    Add files via upload
    - error in importing geckodrivermanager

--- gulong_price_scraper_chrome_test.py
+++ gulong_price_scraper_chrome_test.py
@@ -29,7 +29,6 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import GeckoDriverManager
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
