98c29d9601457abf370e26a480c14ef2ed47e17e
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-08-18 01:53:50 +0800

    Update gulong_price_scraper_lica.py
    np.isnan(float(ar)) for ''

--- gulong_price_scraper_lica.py
+++ gulong_price_scraper_lica.py
@@ -329,7 +329,7 @@ def fix_aspect_ratio(ar):
         return str(remove_exponent(Decimal(str(ar))))
 
 def raw_specs(x):
-    if x['aspect_ratio'] == '':
+    if x['aspect_ratio'] == np.isnan(float(ar)):
         return '/'.join([str(x['width']), '', str(x['diameter'])])
     else:
         return '/'.join([str(x['width']), str(x['aspect_ratio']), str(x['diameter'])])
